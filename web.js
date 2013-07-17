80ce828f55c3dfa057aa064d12b8e3b4335d464b
mihaist <stoicescu@gmail.com>
2013-07-16 21:09:25 -0700

    Responsive design and Css styling

--- web.js
+++ web.js
@@ -1,13 +1,15 @@
 var express = require('express');
 var fs = require('fs');
+var htmlFile = "index.html";
 
 var app = express.createServer(express.logger());
+
 app.get('/', function(request, response) {
-	var buffer = fs.readFileSync("index.html");
-   	response.send(buffer.toString());
+	var html = fs.readFileSync(htmlFile).toString();
+   	response.send(html);
 });
 
-var port = process.env.PORT || 5000;
+var port = process.env.PORT || 8080;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
